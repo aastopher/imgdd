3451769bac1ef9677c3ec48a58a7a37bb5ae36f4
Aaron Stopher <22336995+aastopher@users.noreply.github.com>
2025-01-15 14:13:54 -0700

    test coverage refactor

--- src/validate.rs
+++ src/validate.rs
@@ -5,23 +5,18 @@ use std::path::PathBuf;
 
 #[inline]
 pub fn validate_path(path: &PathBuf) -> PyResult<&PathBuf> {
-
     if !path.exists() {
-        error!("Path does not exist: {}", path.display());
-        return Err(PyValueError::new_err(format!(
-            "Path does not exist: {}",
-            path.display()
-        )));
+        let message = format!("Path does not exist: {}", path.display());
+        error!("{}", message);
+        return Err(PyValueError::new_err(message));
     } else {
         debug!("Path exists: {}", path.display());
     }
 
     if !path.is_dir() {
-        error!("Path is not a directory: {}", path.display());
-        return Err(PyValueError::new_err(format!(
-            "Path is not a directory: {}",
-            path.display()
-        )));
+        let message = format!("Path is not a directory: {}", path.display());
+        error!("{}", message);
+        return Err(PyValueError::new_err(message));
     } else {
         debug!("Path is a directory: {}", path.display());
     }
